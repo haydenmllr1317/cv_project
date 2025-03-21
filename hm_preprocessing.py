e4b50e5235dde08b1312f96f0047605fde9670a6
haydenmllr1317 <hayden_miller@brown.edu>
2025-03-21 09:34:35 +0000

    added empty preprocessing file

---
+++ hm_preprocessing.py
@@ -0,0 +1,8 @@
+# dummy  preprocessing file I haven't written yet
+
+import os
+
+hm_input_path = '/Users/haydenmiller/S6_Edinburgh/CVP/Dataset_filtered'
+hm_output_path = '/Users/haydenmiller/S6_Edinburgh/CVP/preprocessed_data'
+
+
