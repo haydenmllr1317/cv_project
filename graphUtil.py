2f431c3200b169bb1d5112a763a82aa46347a098
Aris <s2751129@ed.ac.uk>
2025-04-02 19:23:41 +0100

    Update Graphs

--- graphUtil.py
+++ graphUtil.py
@@ -23,78 +23,96 @@ def ema_smoothed(input_list : list,ema : float=0.1) -> list:
 	return values_smooth
 
 
-def plot_trainDev_loss(runLog,smoothing=0.1):
+def plot_trainDev_loss(run_log,smoothing=0.1,output_path=None,model_name=""):
 	"""
 	Plots the training and validation loss for a given run
 
 	Args:
-		runLog (dict): Run log dict object
+		run_log (dict): Run log dict object
 		smoothing (float): Smoothing factor to be used
+		output_path (string): Optional save path. If provided, figure is saved instead of shown.
+		model_name (string): Optional name for the model
 	"""
 	plt.figure(figsize=(10, 6))
 
 	#Apply ema smoothing to the loss for cleaner graph
-	lossTrain_smooth=ema_smoothed(runLog["LossTrain"],smoothing)
-	lossDev_smooth=ema_smoothed(runLog["LossDev"],smoothing)
+	lossTrain_smooth=ema_smoothed(run_log["LossTrain"],smoothing)
+	lossDev_smooth=ema_smoothed(run_log["LossDev"],smoothing)
 
 	#Training loss
-	plt.plot(runLog["LossTrain_s"],
+	plt.plot(run_log["LossTrain_s"],
 		lossTrain_smooth,
-		label='Training Loss',
+		label='Training-Set Loss',
 		color='red',
 		alpha=0.8)
 
 	#Dev loss
-	plt.plot(runLog["LossDev_s"],
+	plt.plot(run_log["LossDev_s"],
 		lossDev_smooth,
-		label='Dev Loss',
+		label='Dev-Set Loss',
 		color='blue',
 		alpha=0.8)
 
 	plt.xlabel('Optimizer Steps')
 	plt.ylabel('Loss')
-	plt.title('Training vs Dev Loss')
+	if(len(model_name)>0):
+		model_name=model_name+" "
+	#plt.title(f'{model_name}Loss during Training\n(On Augmented Images)')
+	plt.title(f'{model_name}Loss during Training')
 	plt.legend()
 	plt.grid(True, which='both', linestyle='--', alpha=0.5)
 	plt.tight_layout()
-	plt.show()
+	if(output_path is None):
+		plt.show()
+	else:
+		plt.rcParams.update({'font.size': 14})
+		plt.savefig(output_path,bbox_inches='tight', pad_inches = 0)
 
 
-def plot_trainDev_IoU(runLog,smoothing=0.1):
+def plot_trainDev_IoU(run_log,smoothing=0.1,output_path=None,model_name=""):
 	"""
 	Plots the training and validation IoU for a given run
 
 	Args:
-		runLog (dict): Run log dict object
+		run_log (dict): Run log dict object
 		smoothing (float): Smoothing factor to be used
+		output_path (string): Optional save path. If provided, figure is saved instead of shown.
+		model_name (string): Optional name for the model
 	"""
 	plt.figure(figsize=(10, 6))
 
 	#Apply ema smoothing to the loss for cleaner graph
-	lossTrain_smooth=ema_smoothed(runLog["IoU_Train"],smoothing)
-	lossDev_smooth=ema_smoothed(runLog["IoU_Dev"],smoothing)
+	lossTrain_smooth=ema_smoothed(run_log["IoU_Train"],smoothing)
+	lossDev_smooth=ema_smoothed(run_log["IoU_Dev"],smoothing)
 
 	#Training loss
-	plt.plot(runLog["LossTrain_s"],
+	plt.plot(run_log["LossTrain_s"],
 		lossTrain_smooth,
-		label='Training IoU',
+		label='Training-Set IoU',
 		color='red',
 		alpha=0.8)
 
 	#Dev loss
-	plt.plot(runLog["LossDev_s"],
+	plt.plot(run_log["LossDev_s"],
 		lossDev_smooth,
-		label='Dev IoU',
+		label='Dev-Set IoU',
 		color='blue',
 		alpha=0.8)
 
 	plt.xlabel('Optimizer Steps')
 	plt.ylabel('IoU')
-	plt.title('Training vs Dev IoU')
+	if(len(model_name)>0):
+		model_name=model_name+" "
+	#plt.title(f'{model_name}Intersection over Union score during Training\n(On Augmented Images)')
+	plt.title(f'{model_name}IoU score during Training')
 	plt.legend()
 	plt.grid(True, which='both', linestyle='--', alpha=0.5)
 	plt.tight_layout()
-	plt.show()
+	if(output_path is None):
+		plt.show()
+	else:
+		plt.rcParams.update({'font.size': 14})
+		plt.savefig(output_path,bbox_inches='tight', pad_inches = 0)
 
 
 def plot_IoU_loss_dual(runLog_a,runLog_b,smoothing=0.1):
@@ -154,10 +172,19 @@ def plot_IoU_loss_dual(runLog_a,runLog_b,smoothing=0.1):
 
 def test():
 	import json
-	#with open("Runs/UNet/Run0/runLog.json","r") as f:
-	#	plot_trainDev_loss(json.load(f))
-	with open("Runs/UNet/Run0/runLog.json","r") as f:
-		plot_trainDev_IoU(json.load(f))
+	#U-Net Section
+	if True:
+		with open("Runs/UNet/Run0/runLog.json","r") as f:
+			plot_trainDev_loss(json.load(f),output_path="Runs/UNet/Run0/loss.png",model_name="U-Net")
+		with open("Runs/UNet/Run0/runLog.json","r") as f:
+			plot_trainDev_IoU(json.load(f),output_path="Runs/UNet/Run0/IoU.png",model_name="U-Net")
+
+	#CLIP section
+	if True:
+		with open("Runs/Clip/Run0/runLog.json","r") as f:
+			plot_trainDev_loss(json.load(f),output_path="Runs/Clip/Run0/loss.png",model_name="CLIP")
+		with open("Runs/Clip/Run0/runLog.json","r") as f:
+			plot_trainDev_IoU(json.load(f),output_path="Runs/Clip/Run0/IoU.png",model_name="CLIP")
 
 	#with open("Runs/Clip/Run0/runLog.json","r") as f:
 	#	a=json.load(f)
